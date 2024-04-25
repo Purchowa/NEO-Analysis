f066c97e066a953646b6d008b90fc810c1504af2
Kajetan Nowak <kajtek2n@gmail.com>
2024-04-25 19:43:15 +0200

    feat: check for orbit_determination date in db

--- NEO-Fetch-Post/script.ts
+++ NEO-Fetch-Post/script.ts
@@ -7,6 +7,7 @@ dotenv.config();
 
 const uri: string = process.env.DB_URI || '';
 const client: MongoClient = new MongoClient(uri);
+let pages: number;
 
 async function fetchData(page: number): Promise<NeoApiResponse | null> {
     try {
@@ -30,35 +31,61 @@ async function fetchAndSaveAsteroidsData(start: number, stop: number): Promise<v
             if (data && data.near_earth_objects) {
                 const fetchDate: string = new Date().toISOString().slice(0, 10);
 
-                const documents: NearEarthObject[] = data.near_earth_objects.map(asteroid => ({
-                    ...asteroid,
-                    fetched_on: fetchDate
-                }));
+                for (const asteroid of data.near_earth_objects) {
+                    const asteroidDB = await collection.findOne({ neo_reference_id: asteroid.neo_reference_id });
+                    const document: NearEarthObject = {
+                        ...asteroid,
+                        fetched_on: fetchDate
+                    }
 
-                await collection.insertMany(documents);
+                    if (asteroidDB === null) {
+                        await collection.insertOne(document);
+                        console.log("***Fetched from NASA API and saved new asteroid " + asteroid.name + " to DB from page: " + i + "***");
 
-                console.log("***Fetched from NASA API and saved data to DB from page: " + i + "***");
+                    } else if (asteroidDB!!.orbital_data.orbit_determination_date !== asteroid.orbital_data.orbit_determination_date) {
+
+                        await collection.insertOne(document);
+                        console.log("***Saving new orbit_determination_date for existing asteroid " + asteroidDB.name + "***");
+                    } else {
+                        console.log("***orbit_determination_date for " + asteroid.name + " is not newer - skipping***");
+                    }
+                }
             }
+
         }
     } catch (error) {
         console.error('An error occurred while connecting to the database:', error);
     }
 }
 
-async function start() {
+async function setTotalPages(): Promise<void> {
     try {
+        const response: any = await fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?page=0&size=20&api_key=${process.env.API_KEY}`);
+        const pageJson: NearEarthObject = await response.json();
+        console.log("pageJson:", pageJson);
+        pages = pageJson.page.total_pages;
+    } catch (error) {
+        console.error('An error occurred while fetching or processing data:', error);
+    }
+}
+
+
+async function start(): Promise<void> {
+    try {
+        await setTotalPages();
         await client.connect();
+
+        console.log("Total pages:" + pages);
 
         console.log("***Connected to DB***");
         console.log("***Fetching asteroids data***");
 
-        // Trzeba dostosować, że jeśli orbit_determination_date jest większa przy kolejnym odczycie to należy ją dodać do bazy, w przeciwnym wypadku pominąć asteroidę
         const batchSize = 20;
         const startPages: number[] = [];
-        for (let i = 0; i <= 1000; i += batchSize) {
+        for (let i = 0; i <= pages; i += batchSize) {
             startPages.push(i);
         }
-        
+
         await Promise.all(startPages.map(startPage => {
             const endPage = startPage + batchSize - 1;
             return fetchAndSaveAsteroidsData(startPage, endPage);
@@ -72,4 +99,16 @@ async function start() {
     }
 }
 
+
+
+
+
+
+const test = () => {
+    console.log("Co sekunde");
+}
+//setInterval(start, 1000 * 60 * 2);
+
 start();
+
+
